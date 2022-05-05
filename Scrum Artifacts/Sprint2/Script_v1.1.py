cb8449ddfb1ca14d9a15a1ca3d36ca4fd3d7ff9a
William-png <58230325+William-png@users.noreply.github.com>
2022-05-05 10:57:59 +0200

    Update Script_v1.1.py
    Functies binnen IP-adres-klasse herschreven
    Netwerkadresberekening gecorrigeerd
    Extra subnet voorzien voor netdevices, vlans e.d.
    Ongebruikte code verwijderd

--- Scrum Artifacts/Sprint2/Script_v1.1.py
+++ Scrum Artifacts/Sprint2/Script_v1.1.py
@@ -1,14 +1,12 @@
 #sprint 1
 #class voor conversie van IP-formaat
-from distutils.command.config import config
-import re
+
+#uitgezet wegens foutmelding - William
+#from distutils.command.config import config
+#import re
 
 
-class IP_Adres:
-  def __init__(self):
-    self._decimaal = 0
-
-  def set_dotted(self, ip_adres):
+def set_dotted(ip_adres):
     ip_adres_split = ip_adres.split(".")
     txt = "{:08b}{:08b}{:08b}{:08b}"
     ip_adres_bin = txt.format(int(ip_adres_split[0]),
@@ -16,10 +14,9 @@ class IP_Adres:
       int(ip_adres_split[2]),
       int(ip_adres_split[3]))
     ip_adres_dec = int(ip_adres_bin, 2)
-    self._decimaal = ip_adres_dec
     return ip_adres_dec
   
-  def get_dotted(self, ip_adres_dec):
+def get_dotted(ip_adres_dec):
     # van decimaal naar binair
     txt = "{:032b}"
     ip_adres_bin = txt.format(ip_adres_dec)
@@ -36,12 +33,6 @@ class IP_Adres:
       int(groep_4, 2))
     return ip_adres
   
-  def set_decimal(self, decimaal):
-    self._decimaal = decimaal
-    
-  def get_decimal(self):
-    return self._decimaal
-    
 #basisadres berekenen obv IP & subnetmask
 def network(ip, mask):
 
@@ -61,7 +52,8 @@ def network(ip, mask):
 def bereken_subnets(nw_dec):
   subnets = [nw_dec]
   for i in range(LOKALEN):
-    subnets.append(nw_dec + 64 * i+1)
+    j = i + 1
+    subnets.append(nw_dec + 64 * j)
   return subnets
 
 #subnetmask ifv aantal adressen
@@ -76,11 +68,9 @@ sn_masks_ip = {
 
 # > netwerkadres
 
-NETADR = input('Voer een netwerkadres in: ')
-
+#NETADR = input('Voer een netwerkadres in: ')
 #test zonder input te vragen
-#NETADR = "192.168.0.1"
-#geldigheid testen
+NETADR = "192.168.0.1"
 
 # > aantal lokalen
 
@@ -97,11 +87,6 @@ elif LOKALEN > 2:
   print("U kunt maximum 2 lokalen configureren")
 else:
 
-
-#test zonder input te vragen
-
-# berekeningen
-
 #subnetmask berekenen
   if LOKALEN == 1:
       sn_mask = sn_masks_ip[32]
@@ -111,50 +96,69 @@ else:
       sn_mask = sn_masks_ip[128]
   else:
       sn_mask = sn_masks_ip[256]
+      
+#subnet ip-buffer
+#voor latere ip-berekeningen in andere subnets
+#prefix /26
+sn_mod = 64
 
 # > netwerkadres omzetten naar decimaal
-netadr_dec = IP_Adres().set_dotted(NETADR)
+netadr_dec = set_dotted(NETADR)
 
 # > basisadres
 basisadres = network(NETADR, sn_mask)
 
-# > te voorzien aantal hosts per lokaal
-aantal_hosts = 20
-
-# >> 1 (server) + 1 (router) + aantal lokalen * (20 (hosts) + 1 (switch) + 1 (access point)) + 1 backbone switch indien >4 lokalen
 # > subnets berekenen
 subnets = bereken_subnets(netadr_dec)
 
-# ip-adressen toewijzen
-adressen = {}
-# > 1: router
-router_ip = IP_Adres().get_dotted(subnets[0] + 1)
-# > 2: backbone switch
-bb_switch_ip = IP_Adres().get_dotted(subnets[0] + 2)
-# > 3: server
-server_ip = IP_Adres().get_dotted(subnets[0] + 3)
-# > 4: switch lokaal 1
-#switch1_ip = IP_Adres().get_dotted(subnets[0] + 4)
-# > 5: access point lokaal 1
-#ap1_ip = IP_Adres().get_dotted(subnets[0] + 5)
-# > 6: hosts lokaal 1
-#hosts1_ip = {}
-#for i in range(aantal_hosts):
-#    hosts1_ip["host{0}".format(i)] = IP_Adres().get_dotted(subnets[0] + 6 + i)
+# > te voorzien aantal hosts per lokaal
+aantal_hosts = 20
 
-# > ip-adres per netwerkdevice
-#lokaal1_ip = {
-#    "name" : "Leslokaal 1",
-#    "short_name" : "Lokaal1",
-#    "gateway" : router_ip,
-#    "network_address" : network,
-#    "network_mask" : sn_mask,
-#    "backbone" : bb_switch_ip,
-#    "server" : server_ip,
-#    "switch": switch1_ip,
-#    "ap": ap1_ip,
-#    "hosts": hosts1_ip
-#}
+# ip-adressen netdevices en vlans berekenen
+    
+# > router
+router_ip = get_dotted(subnets[0] + 1)
+# > backbone switch
+bb_switch_ip = get_dotted(subnets[0] + 2)
+# > server
+server_ip = get_dotted(subnets[0] + 3)
+# > management-vlan
+vlan_man_ip = get_dotted(subnets[0] + 4)
+# > access-point-vlan
+vlan_ap_ip = get_dotted(subnets[0] + 5)
+# > lokaal-vlans
+# > kleine L lijkt op cijfer 1
+vlan_l1_ip = get_dotted(subnets[0] + 6)
+vlan_l2_ip = get_dotted(subnets[0] + 7)
+vlan_l3_ip = get_dotted(subnets[0] + 8)
+vlan_l4_ip = get_dotted(subnets[0] + 9)
+vlan_l5_ip = get_dotted(subnets[0] + 10)
+vlan_l6_ip = get_dotted(subnets[0] + 11)
+vlan_l7_ip = get_dotted(subnets[0] + 12)
+vlan_l8_ip = get_dotted(subnets[0] + 13)
+
+#netdevices en vlans toewijzen aan lokaal 0
+
+adressen = {"lokaal0" : {
+    "name" : "Serverlokaal",
+    "short_name" : "Lokaal0",
+    "network_address" : get_dotted(subnets[0]),
+    "gateway" : router_ip,
+    "backbone" : bb_switch_ip,
+    "server" : server_ip,
+    "vlan_man" : vlan_man_ip,
+    "vlan_aps" : vlan_ap_ip,
+    "vlan_l1" : vlan_l1_ip,
+    "vlan_l2" : vlan_l2_ip,
+    "vlan_l3" : vlan_l3_ip,
+    "vlan_l4" : vlan_l4_ip,
+    "vlan_l5" : vlan_l5_ip,
+    "vlan_l6" : vlan_l6_ip,
+    "vlan_l7" : vlan_l7_ip,
+    "vlan_l8" : vlan_l8_ip
+    }
+ }    
+
 #ip-adressen extra lokalen
 for i in range(LOKALEN):
     j = i + 1
@@ -163,29 +167,20 @@ for i in range(LOKALEN):
 #hosts
     hosts_ip = []
     for i in range(aantal_hosts):
-        hosts_ip.append(IP_Adres().get_dotted(subnets[h] + 6 + i))
+        hosts_ip.append(get_dotted(subnets[h] + 3 + i + j * sn_mod))
 
 #andere instellingen
 
     adressen["lokaal{0}_ip".format(j)] = {
     "name" : "Leslokaal {0}".format(j),
     "short_name" : "Lokaal{0}".format(j),
+    "network_address" : get_dotted(subnets[j]),
     "gateway" : router_ip,
-    "backbone" : bb_switch_ip,
-    "server" : server_ip,
-    "network_address" : network,
     "network_mask" : sn_mask,
-    "switch" : IP_Adres().get_dotted(subnets[h] + 1),
-    "ap" : IP_Adres().get_dotted(subnets[h] + 2),
+    "switch" : get_dotted(subnets[h] + 1 + j * sn_mod),
+    "ap" : get_dotted(subnets[h] + 2 + j * sn_mod),
     "hosts" : hosts_ip
     }
-
-#    nwd_ip["Switch 2"] : IP_Adres().get_dotted(subnets[1] + 1)
-#    nwd_ip["Access Point 2"] : IP_Adres().get_dotted(subnets[1] + 2)
-#    for i in range(hosts):
-#        hosts2_ip["host{0}".format(i)] = IP_Adres().get_dotted(subnets[0] + 3 + i)      
-
-# output
 
 # > range voor de hosts
 # > subnet mask
